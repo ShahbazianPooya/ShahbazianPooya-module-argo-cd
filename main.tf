89b425cfe267379a174f5976fc8006f7dd8613b5
Pooya Shahbazian <pooya.shahbazian@gmail.com>
2023-12-26 18:15:19 +0000

    fix

--- main.tf
+++ main.tf
@@ -4,13 +4,14 @@ resource "kubernetes_namespace" "argo-ns" {
   }
 }
 
-resource "helm_release" "argocd" {
-  name       = "msur"
-  chart      = "argo-cd"
-  repository = "https://argoproj.github.io/argo-helm"
-  namespace  = "argocd"
-
-  # We are going to access the console with a port forwarded connection, so we'll disable TLS.
-  # This allow us to avoid the self-signed certificate warning for localhosts.
-  # controller.extraArgs = ["insecure"]
-}
+# resource "helm_release" "argocd" {
+#   name       = "msur"
+#   chart      = "argo-cd"
+#   repository = "https://argoproj.github.io/argo-helm"
+#   namespace  = "argocd"
+  
+#   # We are going to access the console with a port forwarded connection, so we'll disable TLS.
+#   # This allow us to avoid the self-signed certificate warning for localhosts.
+#   # controller.extraArgs = ["insecure"]
+  
+# }
